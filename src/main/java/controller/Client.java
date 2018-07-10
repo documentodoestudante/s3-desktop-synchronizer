071db2c104a9bf18431a05714b94265955724938
Thiago Carreira <thiago.carreira@brodda.com.br>
2018-07-10 12:00:15 -0300

    Screen 2 v1

--- src/main/java/controller/Client.java
+++ src/main/java/controller/Client.java
@@ -1,58 +1,53 @@
 package controller;
 
-import java.util.StringJoiner;
+import java.io.Serializable;
 
-public class Client {
+public class Client implements Serializable {
 
-    private String accessKey;
-    private String secretKey;
-    private String region;
+	private static final long serialVersionUID = 1L;
+	private String accessKey;
+	private String secretKey;
+	private String region;
+	private String name;
+	private String bucket;
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    private String name;
-    private String bucket;
+	public String getAccessKey() {
+		return accessKey;
+	}
 
-    public String getAccessKey() {
-        return accessKey;
-    }
+	public void setAccessKey(String accessKey) {
+		this.accessKey = accessKey;
+	}
 
-    public void setAccessKey(String accessKey) {
-        this.accessKey = accessKey;
-    }
+	public String getSecretKey() {
+		return secretKey;
+	}
 
-    public String getSecretKey() {
-        return secretKey;
-    }
+	public void setSecretKey(String secretKey) {
+		this.secretKey = secretKey;
+	}
 
-    public void setSecretKey(String secretKey) {
-        this.secretKey = secretKey;
-    }
+	public String getRegion() {
+		return region;
+	}
 
-    public String getRegion() {
-        return region;
-    }
+	public void setRegion(String region) {
+		this.region = region;
+	}
 
-    public void setRegion(String region) {
-        this.region = region;
-    }
+	public String getBucket() {
+		return bucket;
+	}
 
-    public String getBucket() {
-        return bucket;
-    }
-
-    public void setBucket(String bucket) {
-        this.bucket = bucket;
-    }
-
-    @Override
-    public String toString() {
-        return new StringJoiner(" - ").add(secretKey).add(accessKey).add(bucket).add(region).toString();
-    }
+	public void setBucket(String bucket) {
+		this.bucket = bucket;
+	}
 }
